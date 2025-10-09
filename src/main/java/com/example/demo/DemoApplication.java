8d04fe8b99d0a7babc84ec4eb43e599e4a2fcf27
bib25 <admin@bitsinbyte.in>
2025-10-09 23:36:32 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import javax.servlet.*;
-import java.lang.Object;
 
 
 @SpringBootApplication
